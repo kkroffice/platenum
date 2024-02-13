1d7e7942457d336f10dbde6a3c05ee02d072caca
Dev sam <lemikan2000@gmail.com>
2024-02-13 23:50:11 +0100

    registered on admin

--- numberplate/admin.py
+++ numberplate/admin.py
@@ -1,4 +1,9 @@
 from django.contrib import admin
-from numberplate.models import AccessToken
+from .models import Address, Contact, Authorities, WorkflowStep, TrespassingLocation, AccessToken
 
 admin.site.register(AccessToken)
+admin.site.register(Address)
+admin.site.register(Contact)
+admin.site.register(Authorities)
+admin.site.register(WorkflowStep)
+admin.site.register(TrespassingLocation)
